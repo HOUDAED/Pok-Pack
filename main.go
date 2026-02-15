1dbbfe1e9c3073127d03a8dcb0b0712ab59e4b81
eryan <ryan.elqali@ynov.com>
2026-02-15 15:42:55 +0100

    dashboard

--- main.go
+++ main.go
@@ -14,17 +14,16 @@ func main() {
 	defer db.Close()
 	log.Println("Base de données initialisée avec succès.")
 
-	
-
 	if err := pok.DeleteAllSessions(); err != nil {
 		log.Printf("Impossible de vider les sessions au démarrage : %v", err)
 	}
-		
+
 	http.HandleFunc("/", pok.HomeHandler)
 	http.HandleFunc("/login", pok.RedirectIfAuthenticated(pok.LoginHandler))
 	http.HandleFunc("/connexion", pok.RedirectIfAuthenticated(pok.LoginHandler))
 	http.HandleFunc("/register", pok.RedirectIfAuthenticated(pok.RegisterHandler))
 	http.HandleFunc("/inscription", pok.RedirectIfAuthenticated(pok.RegisterHandler))
+	http.HandleFunc("/dashboard", pok.DashboardHandler)
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- src/handlers.go
+++ src/handlers.go
@@ -9,3 +9,10 @@ func ConnexionHandler(w http.ResponseWriter, r *http.Request) {
 func InscriptionHandler(w http.ResponseWriter, r *http.Request) {
 	RedirectIfAuthenticated(RegisterHandler)(w, r)
 }
+
+func DashboardHandler(w http.ResponseWriter, r *http.Request) {
+	RequireAuth(func(w http.ResponseWriter, r *http.Request, u User) {
+		http.ServeFile(w, r, "static/dashboard.html")
+	},
+	)(w, r)
+}
